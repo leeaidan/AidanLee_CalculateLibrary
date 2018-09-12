1cc15c48873a27ec37ae48e6cbf0f87e87010605
Aidan Lee <meow123al@gmail.com>
2018-09-12 10:48:08 -0700

    v1.0
    -Added Access Library and Calculate Library
    -Added Square method that squares an integer
    -Added DoMath file that accepts and integer and runs Square code

---
+++ Access Calculate/src/DoMath.java
@@ -0,0 +1,13 @@
+/*Client Code for the Calculate Library
+ * Calls various methods in the calculate library
+ * @author Aidan
+ * @version 0 9/12/2018
+ */
+
+public class DoMath {
+	public static void main(String [] args) {
+		System.out.println(Calculate.square(-3));//returns square of -3 = 9
+		
+	}
+
+}
---
+++ Calculate Library/src/Calculate.java
@@ -0,0 +1,14 @@
+/* This class contains the methods to do various math related tasks.
+ * @author Aidan lee
+ * @version 0 9/12/2018
+ */
+public class Calculate {
+	
+	public static int square(int number) { //returns square of an integer
+		
+		return (number * number);
+	}
+	
+	
+
+}
